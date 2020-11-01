77539ef51ba2a3a2d9e5dae9f2737b0570b461dc
Nancy-Nanneti <73190477+Nancy-Nanneti@users.noreply.github.com>
2020-11-01 17:18:46 +0530

    Create Trader.java

---
+++ Trader.java
@@ -0,0 +1,24 @@
+package labassignment8;
+
+public class Trader {
+	private final String name;
+	private final String city;
+	public String getName() {
+		return name;
+	}
+	public String getCity() {
+		return city;
+	}
+	
+	public Trader(String name, String city) {
+		super();
+		this.name = name;
+		this.city = city;
+	}
+	@Override
+	public String toString() {
+		return "Trader [name=" + name + ", city=" + city + "]";
+	}
+	
+
+}
